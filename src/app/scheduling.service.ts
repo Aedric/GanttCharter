8066632ab04e7a5177fa1c603bb4e14fc6d5d586
Gabriel Elkind <gwelkind@gmail.com>
2018-05-01 20:57:47 -0500

    Add tasks

--- src/app/scheduling.service.ts
+++ src/app/scheduling.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@angular/core';
 
+class ScheduleBlock {
+
+}
+
+class Resource {
+
+}
+
+class Task {
+
+  constructor(
+    public name: string,
+    public dependencies: Task[],
+    public resources: Resource[]
+  ) { }
+
+}
+
 @Injectable()
 export class SchedulingService {
 
