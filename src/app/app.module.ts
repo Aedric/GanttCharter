4e8d152fdc6eb81734d117cf5c73749777440adc
Aedric Donovan <aedric.donovan@genesys.com>
2018-05-01 21:11:14 -0500

    task-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TaskListComponentComponent } from './task-list-component/task-list-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponentComponent
   ],
   imports: [
     BrowserModule
