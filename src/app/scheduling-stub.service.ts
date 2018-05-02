f4973cf9af7fd82455816b2b28b0f37d527e0837
Gabriel Elkind <gwelkind@gmail.com>
2018-05-01 21:10:09 -0500

    Stub service

--- src/app/scheduling-stub.service.ts
+++ src/app/scheduling-stub.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
-import { SchedulingService } from './scheduling.service';
+import { SchedulingService, Task, ScheduledTask } from './scheduling.service';
 
 @Injectable()
 export class SchedulingStubService extends SchedulingService {
 
-  schedule() {
+  _response?: ScheduledTask[];
+  _cb?: (tasks: Task[]) => void;
 
+  scheduleTasks (tasks: Task[]): ScheduledTask[] {
+    if (this._cb ) {
+      this._cb(tasks);
+    }
+    return this._response;
+  }
+
+  anticipateInput (tasks: Task[], response: ScheduledTask[], cb?: (tasks: Task[]) => void) {
+    this._response = response;
+    this._cb = cb;
   }
 
 }
